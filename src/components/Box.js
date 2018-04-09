18ff0e921841d6e03723ac4d8bc467175bd2e75e
Mai Duy Dong <maiduydong94@gmail.com>
2018-04-09 16:40:35 +0700

    box increase

--- src/components/Box.js
+++ src/components/Box.js
@@ -3,10 +3,21 @@ export class Box extends Component {
     constructor(props){
         super(props);
         this.state = {count :10};
+        // setInterval(()=> {
+        //     this.setState({count:this.state.count +1});
+        // }, 1000);
+        this.increase = this.increase.bind(this);
+        
+    }
+    increase(){
+        this.setState({count: this.state.count +1});
     }
     render(){
         return(
-            <p> value :{this.state.count}</p>
+            <div>
+            <p> value ={this.state.count}</p>
+            <button onClick={ this.increase}>Increase</button>
+            </div>                
         );
     }
 }
